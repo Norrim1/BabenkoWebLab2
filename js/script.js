52275d45abd6444fd27cb06d02a408e39afb927e
Norrim1 <danilbabenko76840@gmail.com>
2025-12-25 01:07:44 +0700

    Обратная связь с модальным окном

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ $(function () {
     $('.burger').on('click', function () {
       $('.nav').slideToggle();
     });
+  
+    $('#openModal').on('click', function () {
+      $('.modal').fadeIn();
+    });
+  
+    $('.close, .modal').on('click', function (e) {
+      if (e.target === e.currentTarget) {
+        $('.modal').fadeOut();
+      }
+    });
   });
