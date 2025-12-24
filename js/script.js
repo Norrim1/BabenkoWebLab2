bad15f4bf06654d75cf6b125105f43acfd7db107
Norrim1 <danilbabenko76840@gmail.com>
2025-12-25 01:22:15 +0700

    Загрузка портфолио из json

--- js/script.js
+++ js/script.js
@@ -12,4 +12,32 @@ $(function () {
         $('.modal').fadeOut();
       }
     });
-  });
+
+    $('#feedbackForm').on('submit', function(e) {
+        e.preventDefault();
+
+        const submitBtn = $(this).find('button[type="submit"]');
+        submitBtn.prop('disabled', true).text('В процессе');
+      
+        setTimeout(() => {
+            alert('Отправлено');
+            $(this)[0].reset();
+            $('.modal').fadeOut();
+            submitBtn.prop('disabled', false).text('Отправить');
+        }, 800);
+    });
+
+    $.getJSON('data/portfolio.json', function (data) {
+        const container = $('#portfolioContainer');
+      
+        $.each(data, function (_, item) {
+          container.append(`
+            <div class="portfolio-card">
+                <img src="${item.image}">
+                <h3>${item.title}</h3>
+                <p>${item.description}</p>
+            </div>
+            `);
+        });
+    });      
+});
